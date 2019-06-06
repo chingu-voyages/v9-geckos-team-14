f88d30507c8ff91f09ae7a92b34173089d0e2c7a
NinjaAniket <aniketvnagapure@gmail.com>
2019-06-06 14:35:56 +0530

    user can navigate with the color id

---
+++ client/src/components/Pallete/MainPalette.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./MainPalette.css";
+import ColorShades from "../ColorShades/ColorShades";
+
+export default class MainPalette extends Component {
+  state = {
+    level: 500
+  };
+  render() {
+    const { colorShades } = this.props.palette;
+    // console.log(colors);
+    const displayColors = colorShades[this.state.level].map(color => (
+      <ColorShades background={color.hex} name={color.name} />
+    ));
+    return (
+      <div className="main-pallete">
+        <div className="color-pallete">{displayColors}</div>
+      </div>
+    );
+  }
+}
