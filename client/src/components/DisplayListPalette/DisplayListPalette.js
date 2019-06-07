c590815aa616c30499fad8f8c50f89438f1ccb1d
Pavel Titov <spirokky@yandex.ru>
2019-06-07 13:15:40 +0300

    add login to render

--- client/src/components/DisplayListPalette/DisplayListPalette.js
+++ client/src/components/DisplayListPalette/DisplayListPalette.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import Login from "../Login/Login";
 import { Link } from "react-router-dom";
+
 export default class DisplayListPalette extends Component {
   render() {
     const { listPalettes } = this.props;
     return (
-      <div>
-        {listPalettes.map(palette => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName} </Link>
-          </p>
-        ))}
-      </div>
+      <>
+        <Login />
+        <div>
+          {listPalettes.map(palette => (
+            <p>
+              <Link to={`/palette/${palette.id}`}>{palette.paletteName} </Link>
+            </p>
+          ))}
+        </div>
+      </>
     );
   }
 }
