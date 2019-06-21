be052ee47cb886aeb1abb743e752d15750689d95
Pavel Titov <spirokky@yandex.ru>
2019-06-22 00:00:10 +0300

    DetailPallete first markup

--- client/src/components/PalleteDetail/PalleteDetail.js
+++ client/src/components/PalleteDetail/PalleteDetail.js
@@ -1,21 +1,62 @@
 import React, { Component } from "react";
 import Container from "@material-ui/core/Container";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import "./PalleteDetail.css";
 
-export default class PaletteList extends Component {
+export default class PalleteDetail extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       id: this.props.match.params.id,
-      loading: false
+      name: "Cool Theme Name",
+      author: "",
+      colors: [],
+      loading: false,
+      editing: false
     };
+
+    // fetch pallete from server
+    // update state.loading after
   }
 
   render() {
-    return (
+    const { name, author, colors, loading, editing } = this.state;
+    return loading ? (
+      <div className="pallete-loading">
+        <CircularProgress
+          size={56}
+          color="primary"
+          className="pallete-loading__icon"
+        />
+      </div>
+    ) : (
       <Container maxWidth="lg">
-        <div>Detail pallete id:{this.state.id}</div>
+        <div className="pallete-detail">
+          <div className="pallete-detail__description">
+            <h2>{name}</h2>
+            {author ? <span className="author">by {author}</span> : false}
+          </div>
+          {editing ? (
+            <div className="pallete-detail__colorpicker">color edit</div>
+          ) : (
+            false
+          )}
+          <div className="pallete-detail__colors">
+            {colors.map((color, index) => {
+              return (
+                <div
+                  className="color"
+                  style={{ backgroundColor: color.hex }}
+                  key={index}
+                >
+                  <span className="hex">{`${color.hex}`}</span>
+                  <div className="order">{`${color.order}`}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </Container>
     );
   }
