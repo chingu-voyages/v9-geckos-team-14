f640249fad4744bb549ae9011b0110be9944b1f8
Pavel Titov <spirokky@yandex.ru>
2019-06-21 16:09:44 +0300

    add timestamps to all models

--- models/UserSession.js
+++ models/UserSession.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSessionSchema = new Schema({
-  userId: {
-    type: String,
-    default: ""
+const UserSessionSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      default: ""
+    },
+    deleted: {
+      type: Boolean,
+      default: false
+    }
   },
-  timestamp: {
-    type: Date,
-    default: Date.now()
-  },
-  deleted: {
-    type: Boolean,
-    default: false
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("UserSession", UserSessionSchema);
