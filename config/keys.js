597393ac1ba346121baf2576a17bbd7c34e34df2
Pavel Titov <spirokky@yandex.ru>
2019-06-26 12:39:23 +0300

    get mongo uri from env variable

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://localhost/chingu-color-app",
-  mongoURItest: "mongodb://localhost/chingu-color-app-test"
+  mongoURI: process.env.MONGODB_URI || "mongodb://localhost/chingu-color-app",
+  mongoURItest:
+    process.env.MONGODB_URI_TEST || "mongodb://localhost/chingu-color-app-test"
 };
