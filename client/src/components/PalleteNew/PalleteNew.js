66921eddf63dc20747057ed83b85d88313249615
Pavel Titov <spirokky@yandex.ru>
2019-07-03 14:03:15 +0300

    add submit btn states

--- client/src/components/PalleteNew/PalleteNew.js
+++ client/src/components/PalleteNew/PalleteNew.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
-import Container from "@material-ui/core/Container";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import chroma from "chroma-js";
-import { getUsername } from "../../resources/Helper";
-import "./PalleteNew.css";
+import React, {Component} from 'react';
+import Container from '@material-ui/core/Container';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '@material-ui/core/Button';
+import chroma from 'chroma-js';
+import {getUsername} from '../../resources/Helper';
+import './PalleteNew.css';
 
 export default class PalleteNew extends Component {
   constructor(props) {
@@ -14,25 +15,27 @@ export default class PalleteNew extends Component {
     const randomColor = chroma.random();
 
     this.state = {
-      name: "",
+      name: '',
       author: getUsername(),
       colors: [],
       currentHEX: randomColor.hex(),
-      currentRGB: randomColor.rgb()
+      currentRGB: randomColor.rgb(),
+      loading: false,
+      submitted: false,
     };
   }
 
   handleChange = name => event => {
-    this.setState({ ...this.state, [name]: event.target.value });
+    this.setState({...this.state, [name]: event.target.value});
   };
 
   handleRGB = index => event => {
-    const { currentRGB } = this.state;
+    const {currentRGB} = this.state;
     const value = event.target.value;
     currentRGB[index] = value > 255 ? 255 : value;
     this.setState({
       currentRGB: chroma(currentRGB).rgb(),
-      currentHEX: chroma(currentRGB).hex()
+      currentHEX: chroma(currentRGB).hex(),
     });
   };
 
@@ -44,44 +47,44 @@ export default class PalleteNew extends Component {
 
     if (!chroma.valid(value)) {
       this.setState({
-        currentHEX: value
+        currentHEX: value,
       });
       return;
     }
     this.setState({
-      currentHEX: len === 3 ? value :chroma(value).hex(),
-      currentRGB: chroma(value).rgb()
+      currentHEX: len === 3 ? value : chroma(value).hex(),
+      currentRGB: chroma(value).rgb(),
     });
   };
 
   addColor = hex => event => {
-    const { colors } = this.state;
+    const {colors} = this.state;
     const order = colors.length + 1;
-    colors.push({ hex, order });
-    this.setState({ colors: colors });
+    colors.push({hex, order});
+    this.setState({colors: colors});
   };
 
   generateRandomColor = () => {
     const random = chroma.random();
     this.setState({
       currentHEX: random.hex(),
-      currentRGB: random.rgb()
+      currentRGB: random.rgb(),
     });
   };
 
   submit = event => {
-    const { name, author, colors } = this.state;
-    console.log("body: ", name, author, colors);
-    fetch("/api/palletes/new", {
-      method: "POST",
+    const {name, author, colors} = this.state;
+    console.log('body: ', name, author, colors);
+    fetch('/api/palletes/new', {
+      method: 'POST',
       body: JSON.stringify({
         name: name,
         author: author,
-        colors: colors
+        colors: colors,
       }),
       headers: {
-        "Content-Type": "application/json"
-      }
+        'Content-Type': 'application/json',
+      },
     })
       .then(res => res.json())
       .then(res => {
@@ -90,8 +93,16 @@ export default class PalleteNew extends Component {
   };
 
   render() {
-    const { name, colors, currentHEX, currentRGB } = this.state;
-    const RGB = ["R", "G", "B"];
+    const {
+      name,
+      colors,
+      currentHEX,
+      currentRGB,
+      loading,
+      submitted,
+    } = this.state;
+    const RGB = ['R', 'G', 'B'];
+    const submitBtnText = submitted ? 'Done!' : 'Submit';
     return (
       <Container maxWidth="lg">
         <div className="pallete-detail">
@@ -101,7 +112,7 @@ export default class PalleteNew extends Component {
               id="palleteName"
               label="Name"
               value={name}
-              onChange={this.handleChange("name")}
+              onChange={this.handleChange('name')}
               margin="normal"
             />
           </div>
@@ -119,40 +130,38 @@ export default class PalleteNew extends Component {
                         <InputAdornment position="start">
                           {RGB[index]}
                         </InputAdornment>
-                      )
+                      ),
                     }}
                   />
                 );
               })}
               <TextField
                 className="hex"
-                value={currentHEX.replace("#", "")}
+                value={currentHEX.replace('#', '')}
                 onChange={this.handleHEX}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">#</InputAdornment>
-                  )
+                  ),
                 }}
               />
             </div>
             <div className="color">
               <div
                 className="display"
-                style={{ backgroundColor: `rgb(${currentRGB})` }}
+                style={{backgroundColor: `rgb(${currentRGB})`}}
               />
               <Button
                 className="new-color-btn random"
                 variant="contained"
-                onClick={this.generateRandomColor}
-              >
+                onClick={this.generateRandomColor}>
                 Random
               </Button>
               <Button
                 color="primary"
                 variant="contained"
                 className="new-color-btn addColor"
-                onClick={this.addColor(currentHEX)}
-              >
+                onClick={this.addColor(currentHEX)}>
                 Add Color
               </Button>
             </div>
@@ -174,9 +183,8 @@ export default class PalleteNew extends Component {
                   className="color"
                   style={{
                     backgroundColor: color.hex,
-                    color: fontColor
-                  }}
-                >
+                    color: fontColor,
+                  }}>
                   <span className="hex">{color.hex}</span>
                   <div className="order">{color.order}</div>
                 </div>
@@ -188,9 +196,16 @@ export default class PalleteNew extends Component {
             fullWidth
             disabled={name && colors.length ? false : true}
             size="large"
-            onClick={this.submit}
-          >
-            Submit
+            onClick={this.submit}>
+            {loading ? (
+              <CircularProgress
+                size={24}
+                color="inherit"
+                className="login__loading"
+              />
+            ) : (
+              submitBtnText
+            )}
           </Button>
         </div>
       </Container>
